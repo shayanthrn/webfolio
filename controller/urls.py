39df2f26ce75e0d3306eecdaf527ecb63022ac8b
Hassan Kazemi Tehrani <shayanthrn@gmail.com>
2023-10-25 15:09:01 -0400

    implementing linkedin api

--- controller/urls.py
+++ controller/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 
 urlpatterns = [
-    # path('analyze/', Analyze.as_view(), name='analyze'),
+    path('linkedInimport/', Analyze.as_view(), name='analyze'),
 ]
--- controller/views.py
+++ controller/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+import requests
 
-# Create your views here.
+class Analyze(View):
+    def get(self,request):
+        
+        api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+        linkedin_profile_url = request.GET['linkedinurl']
+        api_key = 'EAqVB3VefS4ds2viuLPFzg'
+        headers = {'Authorization': 'Bearer ' + api_key}
+
+        response = requests.get(api_endpoint,
+                        params={'url': linkedin_profile_url,'skills': 'include'},
+                        headers=headers)
+        print(response.json())
+        return JsonResponse({"requested":"hi"})
