22c1c183a0fd31e292fe020dd380d1383f62f76c
Hassan Kazemi Tehrani <shayanthrn@gmail.com>
2023-11-22 13:26:27 -0500

    imp full functionality #TODO design, import linkedin, recomm

--- controller/views.py
+++ controller/views.py
@@ -116,12 +116,69 @@ class addeducation(View):
             return render(request, 'controller/add-education.html')
         else:
             return redirect("/login/")
+        
+    def post(self,request):
+        if request.user.is_authenticated:
+            school = request.POST.get('school')
+            degree = request.POST.get('degree')
+            field_of_study = request.POST.get('field_of_study')
+            start_date = request.POST.get('startdate')
+            end_date = request.POST.get('enddate')
+            grade = request.POST.get('grade')
+            description = request.POST.get('description')
+
+            new_education = Education(
+                user=request.user,
+                school=school,
+                degree=degree,
+                field_of_study=field_of_study,
+                start_date=start_date,
+                end_date=end_date,
+                grade=grade,
+                description=description
+            )
+
+            new_education.save()
+            return render(request, 'controller/confirm.html')
+        else:
+            return redirect("/login/")
 
 class deleteeducation(View):
     def get(self,request,id):
         if request.user.is_authenticated:
             education = get_object_or_404(Education, pk=id)
             education.delete()
+            return render(request, 'controller/confirm.html')
+        else:
+            return redirect("/login/")
+        
+class editeducation(View):
+    def get(self,request,id):
+        if request.user.is_authenticated:
+            education = get_object_or_404(Education, pk=id)
+            context = {"education": education}
+            return render(request, 'controller/edit-education.html',context=context)
+        else:
+            return redirect("/login/")
+    def post(self,request,id):
+        if request.user.is_authenticated:
+            education = get_object_or_404(Education, id=id)
+            school = request.POST.get('school')
+            start_date = request.POST.get('startdate')
+            end_date = request.POST.get('enddate')
+            degree = request.POST.get('degree')
+            description = request.POST.get('description')
+            field_of_study = request.POST.get('field_of_study')
+            grade = request.POST.get('grade')
+
+            education.school = school
+            education.start_date = start_date
+            education.end_date = end_date
+            education.degree = degree
+            education.description = description
+            education.field_of_study = field_of_study
+            education.grade = grade
+            education.save()
             return render(request, 'controller/confirm.html')
         else:
             return redirect("/login/")
@@ -144,10 +201,70 @@ class deletework(View):
         else:
             return redirect("/login/")
 
+class editwork(View):
+    def get(self, request, id):
+        work = get_object_or_404(Work, pk=id)
+        skills = work.skills.split(',')
+        context = {"work": work, "skills":skills}
+        return render(request, 'controller/edit-workExperience.html', context=context)
+
+    def post(self, request, id):
+        work = get_object_or_404(Work, id=id)
+        work.title = request.POST.get('title')
+        work.employment_type = request.POST.get('employment_type')
+        work.company_name = request.POST.get('company_name')
+        work.start_date = request.POST.get('start_date')
+        work.end_date = request.POST.get('end_date')
+        work.location = request.POST.get('location')
+        work.industry = request.POST.get('industry')
+        work.description = request.POST.get('description')
+        skills = ""
+        for i in request.POST.getlist('skills'):
+            skills += i
+            skills += ","
+        skills = skills[:-1]
+        work.skills = skills
+        work.save()
+
+        return render(request, 'controller/confirm.html')
+
+
 class addwork(View):
     def get(self,request):
         if request.user.is_authenticated:
             return render(request, 'controller/add-WorkExperience.html')
+        else:
+            return redirect("/login/")
+    def post(self,request):
+        if request.user.is_authenticated:
+            title = request.POST.get('title')
+            employment_type = request.POST.get('employment_type')
+            company_name = request.POST.get('company_name')
+            start_date = request.POST.get('start_date')
+            end_date = request.POST.get('end_date')
+            location = request.POST.get('location')
+            industry = request.POST.get('industry')
+            description = request.POST.get('description')
+            skills = ""
+            for i in request.POST.getlist('skills'):
+                skills += i
+                skills += ","
+            skills = skills[:-1]
+            new_work = Work(
+                user=request.user,
+                title=title,
+                employment_type=employment_type,
+                company_name=company_name,
+                start_date=start_date,
+                end_date=end_date,
+                location=location,
+                industry=industry,
+                description=description,
+                skills=skills
+            )
+
+            new_work.save() 
+            return render(request, 'controller/confirm.html')
         else:
             return redirect("/login/")
             
@@ -166,6 +283,28 @@ class addportfolio(View):
             return render(request, 'controller/add-portfolio.html')
         else:
             return redirect("/login/")
+    def post(self,request):
+        if request.user.is_authenticated:
+            name = request.POST.get('name')
+            start_date = request.POST.get('startdate')
+            end_date = request.POST.get('enddate')
+            company_name = request.POST.get('companyname')
+            description = request.POST.get('description')
+            image = request.FILES.get('image')
+
+            portfolio = Portfolio(
+            user= request.user,
+            name=name,
+            start_date=start_date,
+            end_date=end_date,
+            company_name=company_name,
+            description=description,
+            thumbnail=image)
+
+            portfolio.save()
+            return render(request, 'controller/confirm.html')
+        else:
+            return redirect("/login/")
 
 class deleteportfolio(View):
     def get(self,request,id):
@@ -176,6 +315,37 @@ class deleteportfolio(View):
         else:
             return redirect("/login/")
 
+class editportfolio(View):
+    def get(self,request,id):
+        if request.user.is_authenticated:
+            portfolio = get_object_or_404(Portfolio, pk=id)
+            context = {"portfolio": portfolio}
+            return render(request, 'controller/edit-portfolio.html',context=context)
+        else:
+            return redirect("/login/")
+        
+    def post(self,request,id):
+        if request.user.is_authenticated:
+            portfolio = get_object_or_404(Portfolio, id=id)
+            name = request.POST.get('name')
+            start_date = request.POST.get('startdate')
+            end_date = request.POST.get('enddate')
+            company_name = request.POST.get('companyname')
+            description = request.POST.get('description')
+            if 'image' in request.FILES:
+                new_image = request.FILES['image']
+                portfolio.thumbnail = new_image
+
+            portfolio.name = name
+            portfolio.start_date = start_date
+            portfolio.end_date = end_date
+            portfolio.company_name = company_name
+            portfolio.description = description
+            portfolio.save()
+            return render(request, 'controller/confirm.html')
+        else:
+            return redirect("/login/")
+        
 class design(View):
     def get(self,request):
         if request.user.is_authenticated:
