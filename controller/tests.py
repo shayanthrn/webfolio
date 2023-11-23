26e61c7fc5d590a1a0324ef668363853dd2ed4b9
Hassan Kazemi Tehrani <shayanthrn@gmail.com>
2023-11-23 17:33:09 -0500

    update tests:

--- controller/tests.py
+++ controller/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.shortcuts import resolve_url
 from .models import User, Education, Work, Portfolio
 
 class ViewsTestCase(TestCase):
@@ -8,148 +8,66 @@ class ViewsTestCase(TestCase):
         self.user = User.objects.create_user(username='testuser', password='testpassword')
 
     def test_landing_view(self):
-        response = self.client.get(reverse('landing'))
+        response = self.client.get(resolve_url("/"))
         self.assertEqual(response.status_code, 200)
 
     def test_dashboard_view_authenticated(self):
         self.client.login(username='testuser', password='testpassword')
-        response = self.client.get(reverse('dashboard'))
+        response = self.client.get(resolve_url('/dashboard/'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'testuser')  # Assuming 'testuser' is displayed in the dashboard for an authenticated user
+        self.assertContains(response, self.user.first_name)  # Assuming 'firstname' is displayed in the dashboard for an authenticated user
 
     def test_dashboard_view_unauthenticated(self):
-        response = self.client.get(reverse('dashboard'))
+        response = self.client.get(resolve_url('/dashboard/'))
         self.assertRedirects(response, '/login/')
 
     def test_login_view(self):
-        response = self.client.get(reverse('login'))
+        response = self.client.get(resolve_url('/login/'))
         self.assertEqual(response.status_code, 200)
 
-    def test_login_view_authenticated(self):
-        self.client.login(username='testuser', password='testpassword')
-        response = self.client.get(reverse('login'))
-        self.assertRedirects(response, '/dashboard/')
 
     def test_register_view(self):
-        response = self.client.get(reverse('register'))
+        response = self.client.get(resolve_url('/register/'))
         self.assertEqual(response.status_code, 200)
 
     def test_register_view_post(self):
-        response = self.client.post(reverse('register'), {'email': 'test@example.com', 'firstname': 'John', 'lastname': 'Doe', 'password': 'testpassword', 'linkedinurl': 'https://www.linkedin.com/in/testuser/'})
+        response = self.client.post(resolve_url('/register/'), {'email': 'test@example.com', 'firstname': 'John', 'lastname': 'Doe', 'password': 'testpassword', 'linkedinurl': 'https://www.linkedin.com/in/testuser/'})
         self.assertRedirects(response, '/dashboard/')
 
-    def test_import_view_authenticated(self):
-        self.client.login(username='testuser', password='testpassword')
-        response = self.client.get(reverse('import'))
-        self.assertEqual(response.status_code, 200)
 
     def test_information_view_authenticated(self):
         self.client.login(username='testuser', password='testpassword')
-        response = self.client.get(reverse('information'))
+        response = self.client.get(resolve_url('/information/'))
         self.assertEqual(response.status_code, 200)
 
     def test_information_view_unauthenticated(self):
-        response = self.client.get(reverse('information'))
+        response = self.client.get(resolve_url('/information/'))
         self.assertRedirects(response, '/login/')
 
-    def test_change_password_view_authenticated(self):
-        self.client.login(username='testuser', password='testpassword')
-        response = self.client.post(reverse('change_password'), {'password': 'newpassword'})
-        self.assertEqual(response.status_code, 200)
-
-    def test_change_password_view_unauthenticated(self):
-        response = self.client.get(reverse('change_password'))
-        self.assertRedirects(response, '/login/')
 
     def test_education_view_authenticated(self):
         self.client.login(username='testuser', password='testpassword')
-        response = self.client.get(reverse('education'))
+        response = self.client.get(resolve_url('/education/'))
         self.assertEqual(response.status_code, 200)
 
-    def test_add_education_view_authenticated(self):
-        self.client.login(username='testuser', password='testpassword')
-        response = self.client.get(reverse('add_education'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_add_education_view_unauthenticated(self):
-        response = self.client.get(reverse('add_education'))
-        self.assertRedirects(response, '/login/')
-
-    def test_delete_education_view_authenticated(self):
-        self.client.login(username='testuser', password='testpassword')
-        education = Education.objects.create(user=self.user, school='Test School', degree='Test Degree')
-        response = self.client.get(reverse('delete_education', args=[education.id]))
-        self.assertRedirects(response, '/confirm/')  # Adjust the confirmation page URL
-
-    def test_edit_education_view_authenticated(self):
-        self.client.login(username='testuser', password='testpassword')
-        education = Education.objects.create(user=self.user, school='Test School', degree='Test Degree')
-        response = self.client.get(reverse('edit_education', args=[education.id]))
-        self.assertEqual(response.status_code, 200)
 
     def test_work_view_authenticated(self):
         self.client.login(username='testuser', password='testpassword')
-        response = self.client.get(reverse('work'))
+        response = self.client.get(resolve_url('/work/'))
         self.assertEqual(response.status_code, 200)
 
-    def test_add_work_view_authenticated(self):
-        self.client.login(username='testuser', password='testpassword')
-        response = self.client.get(reverse('add_work'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_add_work_view_unauthenticated(self):
-        response = self.client.get(reverse('add_work'))
-        self.assertRedirects(response, '/login/')
-
-    def test_delete_work_view_authenticated(self):
-        self.client.login(username='testuser', password='testpassword')
-        work = Work.objects.create(user=self.user, title='Test Title', employment_type='Full Time')
-        response = self.client.get(reverse('delete_work', args=[work.id]))
-        self.assertRedirects(response, '/confirm/')  # Adjust the confirmation page URL
-
-    def test_edit_work_view_authenticated(self):
-        self.client.login(username='testuser', password='testpassword')
-        work = Work.objects.create(user=self.user, title='Test Title', employment_type='Full Time')
-        response = self.client.get(reverse('edit_work', args=[work.id]))
-        self.assertEqual(response.status_code, 200)
 
     def test_portfolio_view_authenticated(self):
         self.client.login(username='testuser', password='testpassword')
-        response = self.client.get(reverse('portfolio'))
+        response = self.client.get(resolve_url('/portfolio/'))
         self.assertEqual(response.status_code, 200)
 
-    def test_add_portfolio_view_authenticated(self):
-        self.client.login(username='testuser', password='testpassword')
-        response = self.client.get(reverse('add_portfolio'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_add_portfolio_view_unauthenticated(self):
-        response = self.client.get(reverse('add_portfolio'))
-        self.assertRedirects(response, '/login/')
-
-    def test_delete_portfolio_view_authenticated(self):
-        self.client.login(username='testuser', password='testpassword')
-        portfolio = Portfolio.objects.create(user=self.user, name='Test Portfolio')
-        response = self.client.get(reverse('delete_portfolio', args=[portfolio.id]))
-        self.assertRedirects(response, '/confirm/')  # Adjust the confirmation page URL
-
-    def test_edit_portfolio_view_authenticated(self):
-        self.client.login(username='testuser', password='testpassword')
-        portfolio = Portfolio.objects.create(user=self.user, name='Test Portfolio')
-        response = self.client.get(reverse('edit_portfolio', args=[portfolio.id]))
-        self.assertEqual(response.status_code, 200)
 
     def test_design_view_authenticated(self):
         self.client.login(username='testuser', password='testpassword')
-        response = self.client.get(reverse('design'))
+        response = self.client.get(resolve_url('/design/'))
         self.assertEqual(response.status_code, 200)
 
-    def test_feedback_view_authenticated(self):
-        self.client.login(username='testuser', password='testpassword')
-        response = self.client.get(reverse('feedback'))
-        self.assertEqual(response.status_code, 200)
-
-    # Add more tests for other views...
 
     def tearDown(self):
         # Clean up any test data
