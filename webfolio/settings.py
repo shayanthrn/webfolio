07d63e01be3110c6da7b7a02885d55a5c309c627
Hassan Kazemi Tehrani <shayanthrn@gmail.com>
2023-12-01 17:15:15 -0500

    update and change to PostgreSQL

--- webfolio/settings.py
+++ webfolio/settings.py
@@ -76,13 +76,22 @@ WSGI_APPLICATION = 'webfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'webfolio',
+        'USER': 'shayan',
+        'PASSWORD': 'shayan123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
